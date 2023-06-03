dc0b35690c5ac9b9bd5cf0e342b16e2a9c4cbef1
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-06-03 21:12:15 +0300

    com2306

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { fetchBreeds,fetchCatByBreed , API_KEY} from './js/cat-api.js';
+import "./css/style.css";
+
+const refs = {
+     select : document.querySelector('.breed-select'),
+     animalCard : document.querySelector('.cat-info'),
+     alertLoader : document.querySelector('.loader'),
+     alertError: document.querySelector('.error'),
+     catInfo: document.querySelector('.cat-info'),
+}
+refs.select.addEventListener('change', onNewChoiceAnimal);
+refs.alertLoader.classList.add('is-hidden');
+refs.alertError.classList.add('is-hidden');
+
+console.log((refs.animalCard));
+
+
+function onNewChoiceAnimal(event) {
+    const breed_ids = event.target.value;
+    console.log(breed_ids);
+    return fetchCatByBreed(breed_ids);
+       
+};
+
+fetchBreeds(`https://api.thecatapi.com/v1/breeds?${API_KEY}`);
+    
+    
+export { refs};
---
+++ src/js/cat-api.js
@@ -0,0 +1,84 @@
+// const select = document.querySelector('.breed-select');
+// select.addEventListener('change', onNewChoiceAnimal);
+import { refs} from "../index.js";
+
+const API_KEY = 'live_qE21O52c52A37ctUcV6hliaEyBnz59hGiAFTOKXxN2VjhrYLa3zzbYTrw7v0eoiT';
+
+function fetchBreeds(adress) {
+    onShowLoadListAction();
+    return fetchAnimals(adress)
+        .then((response) => {
+            onShowLoadListAction();
+            // console.log(response[0].id);
+            fetchCatByBreed(response[0].id);
+              
+            return renderSelect(response);
+        })
+
+        .catch(() => onShowError());
+       
+}
+function fetchAnimals(adress) {
+    return fetch(adress)
+        .then((response) => {
+            return response.json();
+                           
+        })
+        .catch(() => onShowError());
+        
+}
+    
+
+function renderSelect(animals) {
+    const markup =animals.map(elem => {
+                return `<option value="${elem.id}">${elem.name}</option>`
+            }).join('');
+           
+    return refs.select.insertAdjacentHTML('afterbegin', markup);
+   
+}
+
+
+function fetchCatByBreed(breedId) {
+    console.log(breedId);
+    onShowLoadAnimalAction();
+    return fetchAnimals(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+        .then((response) => {
+            //   console.log(...response);
+            onShowLoadAnimalAction()
+            return renderAnimalCard(...response);
+        })
+        .catch(() => onShowError());
+       
+}
+
+function renderAnimalCard(animal) {
+    
+    const markup = `<img src="${animal.url}" alt='1' width=550>
+    <p><span class='cat-titles'>Назва породи:</span> ${animal.breeds[0].name}</p>
+    <p><span class='cat-titles'>Опис :</span> ${animal.breeds[0].description} </p>
+    <p><span class='cat-titles'>Темперамент:</span> ${animal.breeds[0].temperament}</p>`;
+    return refs.animalCard.innerHTML=markup;
+}
+
+function onShowLoadListAction() {
+    refs.alertLoader.classList.toggle('is-hidden');
+     refs.select.classList.toggle('is-hidden');
+
+}
+
+function onShowLoadListAction() {
+    refs.alertLoader.classList.toggle('is-hidden');
+    refs.select.classList.toggle('is-hidden');
+
+}
+function onShowLoadAnimalAction() {
+    refs.alertLoader.classList.toggle('is-hidden');
+    refs.catInfo.classList.toggle('is-hidden');
+
+}
+function onShowError() {
+    refs.alertError.classList.toggle('is-hidden');
+}
+
+export { fetchBreeds,fetchCatByBreed, API_KEY};
